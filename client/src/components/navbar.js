9d8a15a65abf6e0b982c5cd00b24fadd2522af3b
Lucas Malatchoumy <malatchoumylucas.com>
2023-09-05 15:47:09 +0200

    feat(Routing): create navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import { NavLink } from "react-router-dom";
+import Button from '@mui/material/Button';
+
+function ResponsiveAppBar() {
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <NavLink to="/">
+                <Button 
+                sx={{ my: 2, color: 'white', display: 'block' }}>
+                  Home
+                </Button>
+              </NavLink>
+              <NavLink to="/write">
+                <Button 
+                sx={{ my: 2, color: 'white', display: 'block' }}>
+                  Write a message
+                </Button>
+              </NavLink>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
