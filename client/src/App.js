9313a1a2ad664f32760ff93f85c2b840af062e72
Lucas Malatchoumy <malatchoumylucas.com>
2023-09-05 15:46:53 +0200

    feat(Routing): create routes for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Write from './Write'
+import ResponsiveAppBar from './components/navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ return (
+    <>
+      <ResponsiveAppBar />
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/write" element={<Write />} />
+       </Routes>
+    </>
+ );
+};
 
 export default App;
