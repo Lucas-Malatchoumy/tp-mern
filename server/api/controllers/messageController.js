28c492e06996260c6deef555287807d12a0ec904
Lucas Malatchoumy <malatchoumylucas.com>
2023-09-06 10:31:27 +0200

    feat(logstash): generate winston logs into api response

--- server/api/controllers/messageController.js
+++ server/api/controllers/messageController.js
@@ -1,13 +1,16 @@
 const Message = require("../models/messageModel");
+const logger = require("../../logger")
 
 exports.listAllMessages = (req, res) => {
   Message.find()
   .then((messages) => {
     res.status(200)
+    logger.info(messages)
     res.json(messages)
   })
   .catch((error) => {
-    console.log(error);
+    console.log(error)
+    logger.error(error)
     res.json({message: "Can't get messages"})
   })
 }
@@ -18,12 +21,14 @@ exports.createAMessage = (req, res) => {
   newMessage.save()
   .then((message) => {
     res.status(200)
+    logger.info(message)
     res.json(message)
     return
   })
   .catch((error) => {
     res.status(401)
     console.log(error);
+    logger.error(error)
     res.json({ message: "Invalid request" });
   })
-  }
+}
